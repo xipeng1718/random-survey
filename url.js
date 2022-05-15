9a9064bf0a74ed937f7d39c0296472db1a358805
xipeng1718 <32125978+xipeng1718@users.noreply.github.com>
2022-05-15 14:48:20 +0800

    Update url.js
    updatelink

--- url.js
+++ url.js
@@ -8,7 +8,8 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.surveycake.com/s/Y4kaA',
+	'https://www.surveycake.com/s/bvznK',
+	'https://www.surveycake.com/s/ywoxV',
+	'https://www.surveycake.com/s/QWwGn'
 ];
